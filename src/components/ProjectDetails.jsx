83586556fcdf74c337ce2aa5ba1196e1b35a607c
Pranit2411 <pranitmenkar24@gmail.com>
2025-07-08 14:57:58 -0400

    Deploy Try 3

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -22,7 +22,7 @@ const ProjectDetails = ({title, description, subDescription, image, tags, href})
                             ))}
                         </div>
                         <a href={href} target="_blank" className="inline-flex items-center gap-1 font-medium hover-animation">View Project
-                            <img src="assets/arrow-up.svg" className="size-4" href={href}/>
+                            <img src="assets/arrow-up.svg" className="size-4" href={href} alt="Arrow up"/>
                         </a>
                     </div>
                 </div>
